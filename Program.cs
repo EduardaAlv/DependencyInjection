15ede30fd4e352dd77e98d2c0d42b4a06333b536
Eduarda Alves <65419192+EduardaAlv@users.noreply.github.com>
2025-02-13 19:33:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//Objeto muda a cada vez que é instanciado
+builder.Services.AddTransient<IApplicationBuilder, ApplicationBuilder>();
+
+
+//Objeto só muda por request
+builder.Services.AddScoped<IApplicationBuilder, ApplicationBuilder>();
+
+//Objeto não muda 
+builder.Services.AddSingleton<IApplicationBuilder, ApplicationBuilder>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
